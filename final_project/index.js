051ec2ed59c3156ba4dbe378652a6412067edd85
wo <wogutierrez@gmail.com>
2026-04-04 09:30:14 -0400

    update

--- final_project/index.js
+++ final_project/index.js
@@ -1,19 +1,72 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require('express'); //Done
+const jwt = require('jsonwebtoken'); //Done
+const session = require('express-session') //Done
+
+const customer_routes = require('./router/auth_users.js').authenticated; //Done
+const genl_routes = require('./router/general.js').general; //Done
+
+let users = []
+
+
+
+
+
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+
+
 });
  
+
+// Register a new User
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        //Check if exist already
+        if (!doesExist(username)) {
+            //Add the new uesr to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user. Make sure the username and password was provided"});
+})
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
