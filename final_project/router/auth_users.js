717838fbb44aa89c036ab192636f4066d053d073
wo <wogutierrez@gmail.com>
2026-04-07 11:16:45 -0400

    test

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,14 +9,39 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    //returns boolean 
+    //write code to check if username and password match the one we have in records.
+    let validuser = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+
+    if (validuser.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //checking if username and password were provided
+    if (!username || !password) {
+        return res.status(400).json({message: "Username and password required"});
+    }
+
+    if (authenticatedUser(username, password)) {
+        req.session.isLoggedIn = true;
+        req.session.username = username;
+        return res.status(200).json({message: "login sucessful"});
+    } else {
+        return res.status(401).json({message: "Invalid username or password"});
+    }
+
 });
 
 // Add a book review
