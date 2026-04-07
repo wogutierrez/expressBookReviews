4337c7b0725d437ccf09ca34ef5a1b37b5262982
wo <wogutierrez@gmail.com>
2026-04-07 12:20:19 -0400

    final

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,19 +26,41 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.status(200).json(JSON.stringify(books,null,2));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn; 
+    const book = books[isbn];
+
+    if (book) {
+        res.status(200).json(book);
+    } else {
+        res.status(404).json({message: "Book not found"});
+    }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let booksbyauthor = [];
+
+    for (let id in books) {
+        if(books[id].author.toLowerCase() === author.toLowerCase()){
+            booksbyauthor.push(books[id]);
+        }
+    }
+
+    if (booksbyauthor.length > 0) {    
+        res.status(200).json(booksbyauthor)
+    } else {
+        res.status(404).json({message: "Book by that author were not found"});
+    }
+
 });
 
 // Get all books based on title
