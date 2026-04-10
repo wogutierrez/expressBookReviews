c499fcf5a849a5a31075f1ff256651b9c56f2dbf
wo <wogutierrez@gmail.com>
2026-04-10 11:14:04 -0400

    12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,18 +42,27 @@ public_users.get('/',async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+public_users.get('/isbn/:isbn', async function (req, res) {
+    
     const isbn = req.params.isbn; 
     const book = books[isbn];
 
     if (book) {
-        res.status(200).json(book);
+        try {
+            const bookdetails = await new Promise((resolve) => {
+                resolve(JSON.stringify(book,null,2))
+            }) 
+            res.status(200).json(bookdetails);
+        } catch (error) {
+            res.status(500).json({message: "Error fetching details for this book"})
+        }
+    
     } else {
-        res.status(404).json({message: "Book not found"});
+            res.status(404).json({message: "That book does not exist"})
     }
 
  });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
